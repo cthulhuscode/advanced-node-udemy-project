944a1f35772f7e803dfad52ecba8287557463201
Enrique Ruvalcaba <jordison352@gmail.com>
2023-02-27 21:23:11 -0600

    working on the oauth mechanism for testing

--- test/header.test.js
+++ test/header.test.js
@@ -31,3 +31,31 @@ test("Check if logo appears", async () => {
   const text = await page.$eval("a.brand-logo", (el) => el.innerHTML);
   expect(text).toEqual("Blogster");
 });
+
+test("Clicking login starts oauth flow", async () => {
+  await page.click(".right a");
+  const url = await page.url();
+  expect(url).toMatch(/accounts\.google\.com/);
+});
+
+test("When signed in, shows logout button", () => {
+  const userId = "63ab7419460abd62afa0a210";
+
+  const Buffer = require("safe-buffer");
+  const sessionObject = {
+    passport: {
+      user: userId,
+    },
+  };
+
+  const sessionStr = Buffer.from(JSON.stringify(sessionObject)).toString(
+    "base64"
+  );
+
+  const Keygrip = require("keygrip");
+  const keys = require("../config/keys");
+  const keygrip = new Keygrip([keys.cookieKey]);
+  const sig = keygrip.sign("session=" + sessionStr);
+
+  console.log(sessionStr, sig);
+});
