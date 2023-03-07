e6e72eea8ea6debcbe5512e513c4305c2be02c85
Enrique Ruvalcaba <jordison352@gmail.com>
2023-03-07 12:14:08 -0600

    new tests

--- test/blogs.test.js
+++ test/blogs.test.js
@@ -6,3 +6,33 @@ beforeEach(async () => {
   page = await Page.build();
   await page.goto("http://localhost:3000");
 });
+
+afterEach(async () => {
+  await page.close();
+});
+
+describe("When logged in and clicked new-blog button", () => {
+  beforeEach(async () => {
+    await page.login();
+    await page.click("a.btn-floating");
+  });
+
+  test("Can see blog create form", async () => {
+    const label = await page.getContentsOf("form label");
+    expect(label).toEqual("Blog Title");
+  });
+
+  describe("And using invalid inputs", () => {
+    beforeEach(async () => {
+      await page.click("form button");
+    });
+
+    test("The form shows an error message", async () => {
+      const titleError = await page.getContentsOf(".title .red-text");
+      const contentError = await page.getContentsOf(".content .red-text");
+
+      expect(titleError).toEqual("You must provide a value");
+      expect(contentError).toEqual("You must provide a value");
+    });
+  });
+});
