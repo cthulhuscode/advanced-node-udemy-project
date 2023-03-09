cb2b748bc0e455d2d78dd0b8348b69c413d86b9a
Enrique Ruvalcaba <jordison352@gmail.com>
2023-03-09 13:17:25 -0600

    refactor even more with advanced test helpers

--- test/blogs.test.js
+++ test/blogs.test.js
@@ -62,32 +62,35 @@ describe("When logged in and clicked new-blog button", () => {
 });
 
 describe("User is not logged in", () => {
-  test("User cannot create blog posts", async () => {
-    const result = await page.evaluate(async () => {
-      return fetch("/api/blogs", {
-        method: "POST",
-        credentials: "same-origin",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ title: "My Title", content: "My Content" }),
-      }).then((res) => res.json());
-    });
+  // test("User cannot create blog posts", async () => {
+  //   const result = await page.post("/api/blogs", { title: "T", content: "C" });
+  //   expect(result).toEqual({ error: "You must log in!" });
+  // });
+  // test("User cannot get a list of blog posts", async () => {
+  //   const result = await page.get("/api/blogs");
+  //   expect(result).toEqual({ error: "You must log in!" });
+  // });
 
-    expect(result).toEqual({ error: "You must log in!" });
-  });
+  const actions = [
+    {
+      method: "get",
+      path: "/api/blogs",
+    },
+    {
+      method: "post",
+      path: "/api/blogs",
+      data: {
+        title: "T",
+        content: "C",
+      },
+    },
+  ];
 
-  test("User cannot get a list of blog posts", async () => {
-    const result = await page.evaluate(async () => {
-      return fetch("/api/blogs", {
-        method: "GET",
-        credentials: "same-origin",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }).then((res) => res.json());
-    });
+  test("Blog related actions are prohibited", async () => {
+    const results = await page.execRequests(actions);
 
-    expect(result).toEqual({ error: "You must log in!" });
+    for (let result of results) {
+      expect(result).toEqual({ error: "You must log in!" });
+    }
   });
 });
